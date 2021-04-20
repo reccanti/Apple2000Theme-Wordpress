19acb414235beda81ff0e63611b71d64fdc4168e
Blair Wilcox <reccanti@gmail.com>
2021-04-19 22:36:19 -0400

    Add description

--- template-parts/content-comic.php
+++ template-parts/content-comic.php
@@ -6,10 +6,12 @@
         <?php if (!is_first_webcomic())    : print_r(first_webcomic_link());    endif; ?>
         <?php if (!is_previous_webcomic()) : print_r(previous_webcomic_link()); endif; ?>
         <?php if (!is_next_webcomic())     : print_r(next_webcomic_link());     endif; ?>
-        <?php if (!is_last_webcomic())     : print_r(last_webcomic_link());     endif ?>
+        <?php if (!is_last_webcomic())     : print_r(last_webcomic_link());     endif; ?>
       </nav>
     </div>
     <hr />
-    <h2><?php printf(get_webcomic_collection_title() . " | " . get_the_title()); ?></h2>
+    <h2><?php printf(get_webcomic_collection_title()); ?></h2>
+    <h3><?php printf(get_the_title()); ?></h3>
+    <?php the_content(); ?>
   </div>
 </section>
